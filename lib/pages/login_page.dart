99687b547c0218faaab1e5cba34c80a4c3de4195
brianA1998 <brian.alaniz@hotmail.com>
2021-05-04 17:58:10 -0300

    Terminando de desarrollar el register login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:get/get.dart';
 import 'package:loginflutter/controllers/login_controller.dart';
 
@@ -10,12 +11,13 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: GetBuilder<LoginController>(
         init: LoginController(),
-        builder: (_){
+        builder: (_) {
           return SingleChildScrollView(
-            child: Form(
-              key: controller.formKey,
-              child: Card(
-                child: Padding(padding: const EdgeInsets.all(16.0),
+              child: Form(
+            key: controller.formKey,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -28,38 +30,55 @@ class LoginPage extends StatelessWidget {
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       alignment: Alignment.center,
-
                     ),
-                    SizedBox(height: 50,),
+                    SizedBox(
+                      height: 50,
+                    ),
                     TextFormField(
-                      controller: controller.emailController,
-                      decoration: const InputDecoration(labelText: 'Email'),
-                      validator: (String value){
-                        if(value.isEmpty) return 'Please enter some text';
-                        return null;
-                      }
-                    ),
+                        controller: controller.emailController,
+                        decoration: const InputDecoration(labelText: 'Email'),
+                        validator: (String value) {
+                          if (value.isEmpty) return 'Please enter some text';
+                          return null;
+                        }),
                     TextFormField(
                       controller: controller.passwordController,
                       keyboardType: TextInputType.number,
                       inputFormatters: <TextInputFormatter>[
                         FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-            
                       ],
                       decoration: const InputDecoration(labelText: 'Password'),
-                      validator: (String value){
-                        if(value.isEmpty)
-                            return 'Please enter some text or numbers';
+                      validator: (String value) {
+                        if (value.isEmpty)
+                          return 'Please enter some text or numbers';
                         return null;
-                        
                       },
                       obscureText: true,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      alignment: Alignment.center,
+                      child: SignInButton(Buttons.Email, text: 'Sign In',
+                          onPressed: () async {
+                        _.signInWithEmailAndPassword();
+                      }),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      alignment: Alignment.center,
+                      child: SignInButton(
+                        Buttons.GoogleDark,
+                        text: 'Google',
+                        onPressed: () {
+                          _.signInWithGoogle();
+                        },
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-          )
+          ));
         },
       ),
     );
